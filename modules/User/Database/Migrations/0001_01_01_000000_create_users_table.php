84392419ee8e1618d24b9642f94bf104300e026d
AlexAaqil <alexaaqil.se@gmail.com>
2026-09-02 17:30:54 +0300

    added the user module.

--- modules/User/Database/Migrations/0001_01_01_000000_create_users_table.php
+++ modules/User/Database/Migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->uuid()->unique();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone', 20)->nullable();
+            $table->unsignedTinyInteger('role')->default(3);
+            $table->unsignedTinyInteger('status')->default(1);
+            $table->string('image')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->boolean('is_anonymized')->default(false);
+            $table->timestamp('deleted_at')->nullable();
             $table->timestamps();
         });
 
