9bc01fac309ec0d33bd2baf64c1a2d6477c05557
AlexAaqil <alexaaqil.se@gmail.com>
2026-09-07 15:12:04 +0300

    corrected the routes to allow cashiers to view, create and update orders but can't delete an order.

--- modules/Order/routes.php
+++ modules/Order/routes.php
@@ -3,6 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Order\Http\Controllers\OrderController;
 
+Route::middleware('role:admin,super_admin')->group(function ()
+{
+    Route::prefix('orders')
+        ->name('orders.')
+        ->controller(OrderController::class)
+        ->group(function ()
+    {
+        Route::delete('/{order:uuid}', 'destroy')->name('destroy');
+    });
+});
+
 Route::middleware('role:admin,super_admin,cashier')->group(function ()
 {
     Route::prefix('orders')
@@ -11,21 +22,11 @@ Route::middleware('role:admin,super_admin,cashier')->group(function ()
         ->group(function ()
     {
         Route::get('/', 'index')->name('index');
-    });
-});
-
-Route::middleware('role:admin,super_admin')->group(function ()
-{
-    Route::prefix('orders')
-        ->name('orders.')
-        ->controller(OrderController::class)
-        ->group(function ()
-    {
         Route::get('/create', 'create')->name('create');
         Route::post('/', 'store')->name('store');
         Route::get('/{order:uuid}/edit', 'edit')->name('edit');
         Route::put('/{order:uuid}', 'update')->name('update');
-        Route::delete('/{order:uuid}', 'destroy')->name('destroy');
+        
     });
 });
 
